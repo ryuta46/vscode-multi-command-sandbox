a23df5725073d1b4e6f313abc0d48d6eaad2bca6
ryuta46 <ryuta46g@gmail.com>
2022-07-03 14:26:23 +0900

    add sequantial task with calling vscode command.

--- issue054/test.py
+++ issue054/test.py
@@ -1,14 +1,15 @@
 
 import time
+import datetime
 
 f = open('output.txt', 'w')
-f.write('start.\n')
+f.write(f'start. {datetime.datetime.now()} \n')
 f.close()
 print("doing something...")
 
 time.sleep(5)
 
 f = open('output.txt', 'a')
-f.write('end.\n')
+f.write(f'end. {datetime.datetime.now()}\n')
 f.close()
 
